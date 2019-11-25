00fe309bbce680c73f5fa19c3da76d37dd06ad1e
Jediiah <Jediiah.mail@gmail.com>
2019-11-25 19:30:43 +0100

    Reste Ex5

--- Crypto/Ex4.php
+++ Crypto/Ex4.php
@@ -46,7 +46,7 @@ if(!isset($_POST['phrase4'])) {
     
     <form method="POST">
     <p>Texte à crypter : <br><textarea name="phrase4" cols="60" rows="8" maxlenght="1000"></textarea> </p>
-        <p>Clé de Vigenère : <input type="text" name="cleVigenere" /></p>
+        <p>Clé de Vigenère : <input type="text" name="cleVigenere" maxlength="10" /></p>
         <p>Afficher l'alphabet généré : <input type="checkbox" name="AfficheAlphabet"></p>
         <p><input type="submit" value="Envoyer"></p>
     </form>
@@ -58,8 +58,9 @@ if(!isset($_POST['phrase4'])) {
         $cleVigenere = $_POST["cleVigenere"];
         if(isset($_POST["AfficheAlphabet"])){$affAlpha = TRUE;} else{$affAlpha = FALSE;}
         ?> 
+        <form method="POST">
             <p>Texte à crypter : <br><textarea name="phrase4" cols="60" rows="8" maxlenght="1000"></textarea> </p>
-            <p>Clé de Vigenère : <input type="text" name="cleVigenere" /></p>
+            <p>Clé de Vigenère : <input type="text" name="cleVigenere" maxlength="10" /></p>
             <p>Afficher l'alphabet généré : <input type="checkbox" name="AfficheAlphabet"></p>
             <p><input type="submit" value="Envoyer"></p>
         </form>
