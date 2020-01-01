65da083df49d6505e13088eec720c2efcfffd3ce
Jediiah <Jediiah.mail@gmail.com>
2020-01-01 22:12:13 +0100

    dsfsd

--- TP4/TP4.php
+++ TP4/TP4.php
@@ -1,11 +1,92 @@
 
 <?php
 
+function strlong($texte){
+    /*
+        Idem strlen()
+    */
+    $taille = 0;
+    while(isset($texte[$taille])){$taille++;}
+    return($taille);
+}
+
+
+function arraylong($array){
+    /*
+        Idem count()
+    */
+    $taille = 0;
+    while(isset($array[$taille])){$taille++;}
+    return($taille);
+}
+
+
+function sobstr($string, $carDepart, $long){
+    /*
+        Idem substr()
+    */ 
+    $souschaine = "";
+     for($i=0; $i<$long; $i++){
+        if(isset($string[$i+$carDepart])){
+            $souschaine .= $string[$i+$carDepart];
+        }
+     }
+     return $souschaine;
+}
+
+
+function explose($sep, $chaine){
+    /*
+        Idem explode() 
+    */
+    $listeMots = array();
+    $mot = "";
+    $i = 0;
+
+    while(isset($chaine[$i])){
+        $sousChaineTest = sobstr($chaine, $i, strlong($sep));
+        if($sousChaineTest!=$sep){
+            $mot .= $chaine[$i];
+            if(isset($chaine[$i+1])==FALSE){
+                $motEchange = $mot;
+                $listeMots[arraylong($listeMots)] = $motEchange;
+                $mot = "";
+            }
+        }
+        elseif($sousChaineTest==$sep){
+            $motEchange = $mot;
+            $listeMots[arraylong($listeMots)] = $motEchange;
+            $mot = "";
+        }
+        $i++;
+    }
+    return $listeMots;
+}
+
+
+function tablo_map($fctn, $tablo){
+    /*
+        Idem array_map()
+    */
+    $tabloRetour = array();
+    foreach($tablo as $elmt){
+        $tabloRetour[arraylong($tabloRetour)] = call_user_func($fctn, $elmt);
+    }
+    return $tabloRetour;
+}
+
+
+$str = "abcdef";
+$tablo = ['12', '13', '14', '15'];
+$tablo[6] = '18';
+var_dump($tablo);
+
+
 function compare_temps($temps1, $temps2){
     # on convertit les temps initialement au format str(mm : ss : cc)
     #   au format array(int(mm), int(ss), int(cc))
-    $temps1 = array_map('intval', explode(' : ', $temps1));
-    $temps2 = array_map('intval', explode(' : ', $temps2));
+    $temps1 = tablo_map('intval', explose(' : ', $temps1));
+    $temps2 = tablo_map('intval', explose(' : ', $temps2));
 
     # on convertit en centiemes de secondes
     $temps1 = $temps1[0]*600 + $temps1[1]*100 + $temps1[2];
@@ -15,18 +96,14 @@ function compare_temps($temps1, $temps2){
     return($temps1>$temps2);
 }
 
-/* On utilisera une base XML triée par ordre de départ (listeDepart.xml) et une autre triée par ordre d'arrivée (listeArrive.xml):
-<skieurs>
-    <skieur>
-        <numero_dossard> </numero_dossard>
-        <nom> </nom>
-        <prenom> </prenom>
-        <pays> </pays>
-    </skieur>
-</skieurs>
 
-En plus de sa compatibilité avec PHP grace à SimpleXML ce format permet d'organiser parfaitement 
-le classementet et le fichier texte est facilement lisible.
-https://www.w3schools.com/Php/php_ref_simplexml.asp
-*/
+class Skieur
+{
+    public $dossard = 0;
+    public $nom = "";
+    public $prenom = "";
+    public $pays = "";
+    public $temps = 0;
+
+}
 
