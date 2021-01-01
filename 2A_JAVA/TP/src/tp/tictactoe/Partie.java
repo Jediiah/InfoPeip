bc032ac4520d2c7e6e5550c0d40ffe1e1f79a754
Jediiah <Jediiah.mail@gmail.com>
2021-01-01 19:09:27 +0100

    tictactoe IA finit

--- 2A_JAVA/TP/src/tp/tictactoe/Partie.java
+++ 2A_JAVA/TP/src/tp/tictactoe/Partie.java
@@ -65,18 +65,9 @@ public class Partie {
         return(ReturnString);
     }
 
-    public void TourDeJeu(int Joueur) {
+    private void TourDeJeu(int Joueur) {
         if (this.Mode == 1 && Joueur == -1) {
-            int depth = 0;
-            for (int[] Ligne: this.PlateauInt) {
-                for (int Case: Ligne) {
-                    if (Case == 0) {
-                        depth += 1;
-                    }
-                }
-            }
-            System.out.println(depth);
-            int[] BestMove = TourIA(depth, Joueur);
+            int[] BestMove = TourIA(0, Joueur, -10000, 10000);
             this.PlateauInt[BestMove[0]][BestMove[1]] = Joueur;
         }
         
@@ -103,53 +94,216 @@ public class Partie {
         } 
     }
     
-    public int[] TourIA(int depth, int Joueur) {
-        int[] best = new int[] {-1, -1, -100}; // IA
-        if (Joueur == 1) {
-            best = new int[] {-1, -1, 100}; // Joueur humain
-        }
-
-        if (depth == 0 || this.EtatPartie() != 3) {
-            int score = this.EtatPartie();
+    private int[] TourIA(int depth, int Joueur, int alpha, int beta) {  // Minimax alpha-beta
+        // Cas final
+        int EtatPartie = EtatPartie();
+        if (EtatPartie != 3 || depth >= 5) {
+            if (EtatPartie != 3) {
+                // Si un des joueurs à gagné la valeur du noeud  
+                // doit être +inf ou -inf 
+                return(new int[] {-1, -1, Math.round((EtatPartie*1000)/depth)});
+            }
+            //System.out.println("Commence heuristique");
+            int score = (EvalIA(EtatPartie, depth, Joueur) - EvalIA(EtatPartie, depth, -Joueur)) * Joueur;
+            //System.out.println("finit heuristique");
             return(new int[] {-1, -1, score});
         }
 
-        for (int y=0; y<this.Size; y++) {
-            for (int x=0; x<this.Size; x++) {
-                if (this.PlateauInt[y][x] != 0) {
-                    continue;
-                }
-
-                this.PlateauInt[y][x] = Joueur;
-                int[] score = TourIA(depth-1, -Joueur);
-                this.PlateauInt[y][x] = 0;
-                score[0] = y; score[1] = x;
-                System.out.println(score);
-
-                if (Joueur == -1) {
-                    if (score[2] > best[2]) {
-                        best = score;
-                        if (best[2] == 1) {
-                            System.out.println("break***********************************************************");
-                            break;
-                        }
+        int [] best = new int[] {-1, -1, 0};// v=best[2] Wiki
+        if (Joueur == -1) {
+            best[2] = 1000; 
+            for (int y=0; y<this.Size; y++) {
+                for (int x=0; x<this.Size; x++) {
+                    if (this.PlateauInt[y][x] != 0) {continue;}
+                    
+                    this.PlateauInt[y][x] = Joueur;
+                    int[] score = TourIA(depth+1, -Joueur, alpha, beta);
+                    if (beta > score[2]) {
+                        beta = score[2];
+                        best[0] = y; best[1] = x;
                     }
-                }
-                else {
-                    if (score[2] < best[2]) {
-                        best = score;
-                        if (best[2] == -1) {
-                            System.out.println("break----------------------------------------------------------");
-                            break;
-                        }
+                    this.PlateauInt[y][x] = 0;
+                    if (alpha >= beta) {
+                        best[2] = beta;
+                        return(best);
                     }
                 }
             }
+            best[2] = beta;
+            return(best);
+        }
+
+        else if (Joueur == 1) {
+            best[2] = -1000;
+            for (int y=0; y<this.Size; y++) {
+                for (int x=0; x<this.Size; x++) {
+                    if (this.PlateauInt[y][x] != 0) {continue;}
+
+                    this.PlateauInt[y][x] = Joueur;
+                    int[] score = TourIA(depth+1, -Joueur, alpha, beta);
+                    if (alpha < score[2]) {
+                        alpha = score[2];
+                        best[0] = y; best[1] = x;
+                    }
+                    this.PlateauInt[y][x] = 0;
+                    if (beta <= alpha) {
+                        best[2] = alpha;
+                        return(best);
+                    }
+                }
+            }
+            best[2] = alpha;
+            return(best);
         }
         return(best);
     }
 
-    public int EtatPartie(){
+    private int EvalIA(int EtatPartie, int depth, int Joueur) {
+        boolean IsForkPossible = true;
+        int CountPossibleForks = 0;
+        int TotalJoueur = 0;
+        // On regarde chaque ligne et ajoute 10 au total par case Joueur 
+        // si l'autre joueur n'a pas de case sur cette ligne
+        for (int[] Ligne: this.PlateauInt) {
+            int SommeLigne = 0;
+            for (int Valeur: Ligne) {
+                if (Valeur == Joueur) {
+                    SommeLigne += 10;
+                }
+                else if (Valeur == -Joueur) {
+                    SommeLigne = 0;
+                    break;
+                }
+            }
+            TotalJoueur += SommeLigne;
+            if (SommeLigne == (this.Size-1)*10) {IsForkPossible = false;}
+            if (SommeLigne == (this.Size-2)*10) {CountPossibleForks++;}
+        }
+
+        // Idem pour les colonnes
+        for (int x=0; x<this.Size; x++) {
+            int SommeColonne = 0;
+            for (int y=0; y<this.Size; y++) {
+                if (this.PlateauInt[y][x] == Joueur) {
+                    SommeColonne += 10;
+                }
+                else if (this.PlateauInt[y][x] == -Joueur) {
+                    SommeColonne = 0;
+                    break;
+                }
+            }
+            TotalJoueur += SommeColonne;
+            if (SommeColonne == (this.Size-1)*10) {IsForkPossible = false;}
+            if (SommeColonne == (this.Size-2)*10) {CountPossibleForks++;}
+        }
+
+        // Idem pour les diagonales
+        int SommeDiag0 = 0;
+        int SommeDiag1 = 0;
+        // Diagonale0
+        for (int i=0; i<this.Size; i++) {
+            if (this.PlateauInt[i][i] == Joueur) {
+                SommeDiag0 += 10;
+            }
+            else if (this.PlateauInt[i][i] == -Joueur) {
+                SommeDiag0 = 0;
+                break;
+            } 
+        }
+        // Diagonale1
+        for (int i=0; i<this.Size; i++) {
+            if (this.PlateauInt[i][this.Size-1-i] == Joueur) {
+                SommeDiag1 += 10;
+            }
+            else if (this.PlateauInt[i][this.Size-1-i] == -Joueur) {
+                SommeDiag1 = 0;
+                break;
+            }
+        }
+        TotalJoueur += SommeDiag0+SommeDiag1;
+        if (SommeDiag0 == (this.Size-1)*10 || SommeDiag1 == (this.Size-1)*10) {IsForkPossible = false;}
+        if (SommeDiag0 == (this.Size-2)*10) {CountPossibleForks++;}
+        if (SommeDiag1 == (this.Size-2)*10) {CountPossibleForks++;}
+
+        // On regarde si le joueur peut faire une fourchette au prochain coup
+        // (il peut faire 2 lignes complètes après la fourchette)
+        // Une fourchette possible ajoute 100 au total
+        if (IsForkPossible && CountPossibleForks >= 2) {
+            if (ChercherFourchettes(Joueur)) {TotalJoueur += 100;}
+        }
+        return(TotalJoueur/depth);
+    }
+
+    private boolean ChercherFourchettes(int Joueur) {
+        for (int y=0; y<this.Size; y++) {
+            for (int x=0; x<this.Size; x++) {
+                if (this.PlateauInt[y][x] != 0) {continue;}
+
+                this.PlateauInt[y][x] = Joueur;
+                int NbrWinProchainCoup = 0;
+                for (int[] Ligne: this.PlateauInt) {
+                    int SommeLigne = 0;
+                    for (int Valeur: Ligne) {
+                        if (Valeur == Joueur) {
+                            SommeLigne += 10;
+                        }
+                        else if (Valeur == -Joueur) {
+                            SommeLigne = 0;
+                            break;
+                        }
+                    }
+                    if (SommeLigne == (this.Size-1)*10) {NbrWinProchainCoup++;}
+                }
+        
+                // Idem pour les colonnes
+                for (int i=0; i<this.Size; i++) {
+                    int SommeColonne = 0;
+                    for (int j=0; j<this.Size; j++) {
+                        if (this.PlateauInt[j][i] == Joueur) {
+                            SommeColonne += 10;
+                        }
+                        else if (this.PlateauInt[j][i] == -Joueur) {
+                            SommeColonne = 0;
+                            break;
+                        }
+                    }
+                    if (SommeColonne == (this.Size-1)*10) {NbrWinProchainCoup++;}
+                }
+        
+                // Idem pour les diagonales
+                int SommeDiag0 = 0;
+                int SommeDiag1 = 0;
+                // Diagonale0
+                for (int i=0; i<this.Size; i++) {
+                    if (this.PlateauInt[i][i] == Joueur) {
+                        SommeDiag0 += 10;
+                    }
+                    else if (this.PlateauInt[i][i] == -Joueur) {
+                        SommeDiag0 = 0;
+                        break;
+                    } 
+                }
+                // Diagonale1
+                for (int i=0; i<this.Size; i++) {
+                    if (this.PlateauInt[i][this.Size-1-i] == Joueur) {
+                        SommeDiag1 += 10;
+                    }
+                    else if (this.PlateauInt[i][this.Size-1-i] == -Joueur) {
+                        SommeDiag1 = 0;
+                        break;
+                    }
+                }
+                if (SommeDiag0 == (this.Size-2)*10) {NbrWinProchainCoup++;}
+                if (SommeDiag1 == (this.Size-2)*10) {NbrWinProchainCoup++;}
+
+                this.PlateauInt[y][x] = 0;
+                if (NbrWinProchainCoup >= 2) {return(true);}
+            }
+        }
+        return(false);
+    }
+
+    private int EtatPartie(){
         // rend le numéro du joueur gangnant s'il y en a un, 3 si la partie est bloquée, 0 si tout va bien.
         // Test de gagnat sur les lignes
         for (int[] Ligne: this.PlateauInt) {
@@ -204,7 +358,7 @@ public class Partie {
         return(3);
     }
 
-    public boolean IsPositionCorrect(int PosX, int PosY) {
+    private boolean IsPositionCorrect(int PosX, int PosY) {
         // PosX et PosY sont les positions x y courament utilisées le plateau utlise donc Plateau[PosY][PosX]
         if (PosX >= this.Size || PosY >= this.Size || PosX < 0 || PosY < 0) {
             return(false);
